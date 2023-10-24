d44d865422080073d1d5a0e1b5af39ef09606930
KostyaSt2022 <kotekostya@gmail.com>
2023-10-24 19:03:22 +0300

    ДЗ - Урок 5. От простого к практике

---
+++ hw/task_5/service/StudentGroupService.java
@@ -0,0 +1,22 @@
+package task_5.service;
+
+import task_5.model.Student;
+import task_5.model.StudentGroup;
+import task_5.model.Teacher;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+public class StudentGroupService {
+    private final StudentGroup studentGroup = new StudentGroup();
+    private Integer maxGroupId = 0;
+    public void createStudentGroup(Teacher teacher, List<Student> studentList){
+        studentGroup.setGroupId(++maxGroupId);
+        studentGroup.setTeacher(teacher);
+        studentGroup.setStudentList(studentList);
+    }
+    public StudentGroup getStudentGroup() {
+        return studentGroup;
+    }
+}
